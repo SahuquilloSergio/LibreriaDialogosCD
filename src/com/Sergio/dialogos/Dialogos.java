465c0371bc4ce71fd155af0555dd02095aa79dfd
serxa <serxa@LAPTOP-82OG6QUN>
2018-01-19 10:40:12 +0100

    Añadido el metodo YESNO que permite hacer una pregrunta personalizada de tipo SI/NO con titulo y respuestas personaliable.

--- src/com/Sergio/dialogos/Dialogos.java
+++ src/com/Sergio/dialogos/Dialogos.java
@@ -1,9 +1,26 @@
 package com.Sergio.dialogos;
 
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import java.util.Scanner;
+
 /**
  *
  * @author serxa
  */
 public class Dialogos {
+    Scanner sc = new Scanner(System.in);
     
+    public static void YESNO(){
+    String Pregunta = JOptionPane.showInputDialog("Introduce la Pregunta");
+    String Titulo = JOptionPane.showInputDialog("Introduce el Titulo");
+       int ele = JOptionPane.showOptionDialog(new JFrame(), Pregunta, Titulo, JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,new Object[]{"Yes", "No"}, JOptionPane.YES_OPTION);
+       if (ele ==JOptionPane.YES_OPTION){
+           
+       }else if (ele == JOptionPane.NO_OPTION){
+           JOptionPane.showMessageDialog(null,"Has Fallado");
+       }else if (ele == JOptionPane.CLOSED_OPTION){
+           JOptionPane.showMessageDialog(null, "Hasta la Próxima");
+       }
+    }
 }
